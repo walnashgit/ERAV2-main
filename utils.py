6741971614db89f819318b4d8e865c7579e32210
Avinash Agarwal <73463300+walnashgit@users.noreply.github.com>
2024-04-14 17:45:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torchsummary import summary
-from ERAV2_main.main import *
+from Util.CIFAR10DataSet import CIFAR10AlbumenationDataSet
+from Util.main import *
 from torch_lr_finder import LRFinder
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,25 +10,13 @@ from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, Ablat
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from torchvision import datasets, transforms
-import torchvision
 
-
-class CIFAR10AlbumenationDataSet(torchvision.datasets.CIFAR10):
-    def __init__(self, root: str, train=True, download=True, transform=None):
-        super().__init__(root=root, train=train, download=download, transform=transform)
-
-    def __getitem__(self, index):
-        image, label = self.data[index], self.targets[index]
-
-        if self.transform is not None:
-            transformed = self.transform(image=image)
-            image = transformed["image"]
-
-        return image, label
-
+mean = (0.4914, 0.4822, 0.4465)
+std_dev = (0.2471, 0.2435, 0.2616)
 
 
 class CIFAR10ResNetUtil:
+
     def __init__(self, seed=1):
         self.cuda = False
         self.use_mps = False
@@ -52,8 +41,8 @@ class CIFAR10ResNetUtil:
             A.RandomCrop(32, 32),
             A.HorizontalFlip(p=0.5),
             A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8,
-                            fill_value=[0.4914, 0.4822, 0.4465]),
-            A.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616)),
+                            fill_value=list(mean)),
+            A.Normalize(mean, std_dev),
             ToTensorV2()
         ])
 
@@ -61,6 +50,12 @@ class CIFAR10ResNetUtil:
         return A.Compose([
             A.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616)),
             ToTensorV2()
+        ])
+
+    def get_test_transform_cifar10_resnet_2(self):
+        return transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
     def get_train_set_cifar10(self, train_transforms=None):
@@ -111,6 +106,12 @@ class CIFAR10ResNetUtil:
         return lr_finder.plot(log_lr=True)[-1]
 
     @staticmethod
+    def denormalize(img):
+        for i in range(img.shape[0]):
+            img[i] = (img[i] * std_dev[i]) + mean[i]
+        return img
+
+    @staticmethod
     def start_training_testing(epochs, collect_images, model, device, train_loader,
                                test_loader, optimizer, criterion, scheduler=None):
         model = model.to(device)
@@ -122,21 +123,34 @@ class CIFAR10ResNetUtil:
                 scheduler.step()
             test(model, device, test_loader, epoch, epochs, collect_images, criterion)
 
-    @staticmethod
-    def plot_miss_classified_images(train_set, device, images=missclassified_images, num_images=10):
+    def plot_miss_classified_images(self, train_set, num_images=10):
+        self.plot_images(train_set, missclassified_images, num_images, False)
+
+    def plot_images(self, train_set, images, num_images=10, true_image=True):
         fig = plt.figure(figsize=(5, 5))
         fig.subplots_adjust(wspace=0.8, hspace=0.8)
         for i in range(min(num_images, len(images))):
             image, true_label, predicted_label = images[i]
             ax = fig.add_subplot(5, 2, i + 1)
+            image = CIFAR10ResNetUtil.denormalize(image)
             ax.imshow(np.transpose(image, (1, 2, 0)))
-            ax.set_title(f'True: {train_set.classes[true_label]}, \n Predicted: {train_set.classes[predicted_label]}')
+            if true_image:
+                ax.set_title(f'{train_set.classes[true_label]}')
+            else:
+                ax.set_title(f'True: {train_set.classes[true_label]}, \n Predicted: {train_set.classes[predicted_label]}')
             ax.axis('off')
-        if device == 'mps':
+        if self.use_mps:
             plt.show()
 
-    @staticmethod
-    def plot_training_and_test_loss_and_accuracy(device):
+    def visualize_images(self, train_set, num_images=10):
+        dataiter = iter(train_set)
+        images = []
+        for i in range(num_images):
+            imgs, labels = next(dataiter)
+            images.append((imgs, labels, None))
+        self.plot_images(train_set=train_set, images=images)
+
+    def plot_training_and_test_loss_and_accuracy(self):
         # Plot the training and test loss and accuracy
         t = [t_items.item() for t_items in train_losses]
         fig, axs = plt.subplots(2, 2, figsize=(15, 10))
@@ -148,53 +162,21 @@ class CIFAR10ResNetUtil:
         axs[0, 1].set_title("Test Loss")
         axs[1, 1].plot(test_acc)
         axs[1, 1].set_title("Test Accuracy")
-        if device == 'mps':
+        if self.use_mps:
             plt.show()
 
-    # @staticmethod
-    # def show_grad_cam_heatmap(model, images=missclassified_images, num_images=10):
-    #     target_layers = [model.layer4[-1]]
-    #
-    #     cam = GradCAM(model=model, target_layers=target_layers)
-    #     # targets is the object/class that we are looking for in the input image.
-    #     # input_tensor is the input image where we are searching for the object.
-    #     target_images = []
-    #     targets = []
-    #
-    #     for i in range(min(num_images, len(images))):
-    #         image, true_label, predicted_label = images[i]
-    #         targets = [ClassifierOutputTarget(predicted_label)]
-    #         target_images.append(image)
-    #
-    #     input_tensor = torch.stack(target_images)
-    #
-    #     grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
-    #
-    #     grayscale_cam = grayscale_cam[0, :]
-    #     visualization = show_cam_on_image(np.transpose(target_images, (1, 2, 0)), grayscale_cam, use_rgb=True)
-
-    @staticmethod
-    def show_grad_cam_heatmap(model, train_set, images=missclassified_images, num_images=10):
+    def show_grad_cam_heatmap(self, model, train_set, images=missclassified_images, num_images=10):
         target_layers = [model.layer4[-1]]
 
         fig = plt.figure(figsize=(8, 8))
         fig.subplots_adjust(wspace=0.8, hspace=0.8)
 
-        preprocess = transforms.Compose([
-            transforms.ToPILImage(),
-            # Add any additional preprocessing steps here, e.g., resizing, normalization
-            transforms.Resize((32, 32)),  # Resize to match ResNet18 input size
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            # Normalize with ImageNet stats
-        ])
+        # Create GradCAM object
+        cam = GradCAM(model=model, target_layers=target_layers)
 
         for i, (image, true_label, predicted_label) in enumerate(images[:num_images]):
 
-            input_tensor = preprocess(image).unsqueeze(0)  # Add batch dimension
-
-            # Create GradCAM object
-            cam = GradCAM(model=model, target_layers=target_layers)
+            input_tensor = image.unsqueeze(0)  # Add batch dimension
 
             # Specify target classes
             target = [ClassifierOutputTarget(predicted_label.item())]
@@ -205,19 +187,11 @@ class CIFAR10ResNetUtil:
             # Visualize CAMs
             grayscale_cam = grayscale_cam[0, :]  # Select the CAM for the first (and only) image in the batch
 
+            image = self.denormalize(image)
             image = np.transpose(image, (1, 2, 0))
-
             image = np.array(image)
-            image = image.astype(np.float32) / 255.0
-
-            visualization = show_cam_on_image(image, grayscale_cam, use_rgb=True)
-
-            # Plot the visualization
-            # plt.figure(figsize=(8, 8))
-            # plt.subplot(2, 5, i + 1)
-            # plt.imshow(visualization)
-            # plt.title(f'True Label: {train_set.classes[true_label]}, Predicted Label: {train_set.classes[predicted_label]}')
-            # plt.axis('off')
+            image = image.astype(np.float32)
+            visualization = show_cam_on_image(image, grayscale_cam, use_rgb=True, image_weight=0.8)
 
             ax = fig.add_subplot(5, 2, i + 1)
             ax.imshow(visualization)
